7ab8920e5daee3fad92d834b36e2c731c90b06ec
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-02-05 14:15:30 +0100

    Add user edit view

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,4 +1,42 @@
 <x-app-layout>
 
-    Edit
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit user') }}
+        </h2>
+    </x-slot>
+
+    <div>
+        <div class="max-w-2xl mx-auto py-10 sm:px-6 lg:px-8">
+
+            {{-- Name --}}
+            <div>
+                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                <div class="mt-1">
+                    <input type="text" name="name" id="name" value="{{$user->name}}" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                </div>
+            </div>
+
+            {{-- Email --}}
+            <div class="mt-4">
+                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <div class="mt-1">
+                    <input type="text" name="email" id="email" value="{{$user->email}}" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" >
+                </div>
+            </div>
+
+            {{-- Save/Cancel --}}
+            <div class="pt-5">
+                <div class="flex justify-end">
+                    <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Cancel
+                    </button>
+                    <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Save
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,5 +21,8 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 };
